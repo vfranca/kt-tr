c731943282951cf8c20efbd7727e55aeaf328191
Valmir Franca <vfranca3@gmail.com>
2021-03-16 21:24:21 -0300

    Removido a implementacao do sys

--- kt_tr/kt_tr.py
+++ kt_tr/kt_tr.py
@@ -1,6 +1,3 @@
-import sys
-
-
 topo = 0.0
 fundo = 0.0
 
@@ -27,19 +24,3 @@ def metade(range):
 
 def meio(topo, metade):
     return topo - metade
-
-
-if __name__ == "__main__":
-    if len(sys.argv) > 2:
-        topo = float(sys.argv[1])
-        fundo = float(sys.argv[2])
-    range = range(topo, fundo)
-    terco = terco(range)
-    terco_sup = terco_sup(topo, terco)
-    terco_inf = terco_inf(fundo, terco)
-    metade = metade(range)
-    meio = meio(topo, metade)
-
-    print("%.2f" % terco_sup)
-    print("%.2f" % meio)
-    print("%.2f" % terco_inf)

4546457e4362e266ef91e531c97ee47177830b9c
Valmir Franca <vfranca3@gmail.com>
2021-03-16 21:20:44 -0300

    Implementado click

--- kt_tr/cli.py
+++ kt_tr/cli.py
@@ -1,18 +1,23 @@
 from kt_tr import kt_tr
-import sys
+import click
 
 
-def cli():
-    if len(sys.argv) > 2:
-        topo = float(sys.argv[1])
-        fundo = float(sys.argv[2])
+@click.command()
+@click.argument("topo")
+@click.argument("fundo")
+def cli(topo, fundo):
+    topo = float(topo)
+    fundo = float(fundo)
     range = kt_tr.range(topo, fundo)
     terco = kt_tr.terco(range)
     terco_sup = kt_tr.terco_sup(topo, terco)
     terco_inf = kt_tr.terco_inf(fundo, terco)
     metade = kt_tr.metade(range)
     meio = kt_tr.meio(topo, metade)
+    click.echo("%.2f" % terco_sup)
+    click.echo("%.2f" % meio)
+    click.echo("%.2f" % terco_inf)
 
-    print("%.2f" % terco_sup)
-    print("%.2f" % meio)
-    print("%.2f" % terco_inf)
+
+if __name__ == "__main__":
+    cli()

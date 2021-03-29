ce882b4c8d475e5b390646e45fd1df32dbf656a2
Valmir Franca <vfranca3@gmail.com>
2021-03-29 10:04:58 -0300

    Adicionados movimentos projetados de alta e de baixa

--- kt_tr/cli.py
+++ kt_tr/cli.py
@@ -1,3 +1,6 @@
+"""
+Scripts do console
+"""
 from kt_tr import kt_tr
 import click
 
@@ -14,9 +17,14 @@ def cli(topo, fundo):
     terco_inf = kt_tr.terco_inf(fundo, terco)
     metade = kt_tr.metade(range)
     meio = kt_tr.meio(topo, metade)
+    alta = kt_tr.mm_alta(topo, range)
+    baixa = kt_tr.mm_baixa(fundo, range)
+    click.echo("%.2f" % alta)
     click.echo("%.2f" % terco_sup)
     click.echo("%.2f" % meio)
     click.echo("%.2f" % terco_inf)
+    click.echo("%.2f" % baixa)
+    return 0
 
 
 if __name__ == "__main__":
